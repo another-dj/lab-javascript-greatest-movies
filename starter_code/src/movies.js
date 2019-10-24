3959e009d8d73baa85a9e0314ee20bb75ab1a098
alexis <alexis@DESKTOP-AJ1CK7K.localdomain>
2019-10-24 15:51:04 +0100

    first commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,11 +2,66 @@
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals
 
+function calculateAverageMovieRate(arrayOfMovies) {
+   /* let rates = 0; 
+    for (i=0; i<arrayOfMovies.length; i++) {
+         rates = rates + arrayOfMovies[i].rate;
+         let divide = rates/arrayOfMovies.length;
+         return divide;
+}*/
+let rates = arrayOfMovies.reduce((accumulator, value, index, array) => {
+return accumulator + value["rate"];
+},0);
+let average = rates/arrayOfMovies.length;
+return average;
+}
+
+calculateAverageMovieRate(MOVIES);
+
 // Iteration 2: Drama movies - Get the average of Drama Movies
+
+function calculateAverageDramaRate(arrayOfMovies) {
+    const filterDrama = arrayOfMovies.filter((value) => {
+      return value["genre"].includes("Drama")
+    
+    });
+    return calculateAverageMovieRate(filterDrama) || 0;
+    }
+    console.log(calculateAverageDramaRate(MOVIES));
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
+function orderByYear(arrayOfMovies) {
+    let ordered = arrayOfMovies.sort((a,b) => {
+        if (a.year === b.year && a.title > b.title) {
+            return 1;
+        }
+        else if (a.year === b.year && a.title < b.title) {
+            return -1;
+        }
+        else if (a.year > b.year) {
+            return 1;
+        }
+        else if (a.year < b.year){
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    });
+return ordered;
+}
+
+
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
+
+function countSpielbergDramaMovies(arrayOfMovies) {
+    const dramaSpiel = arrayOfMovies.filter((a) => {
+        if ((a.director) && 
+    }
+    
+    )
+}
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
